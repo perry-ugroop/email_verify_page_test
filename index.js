b7f59490f2e48eb8c8c4255d5f7ad03178da1235
Perry Valdez <perry.valdez@ugroop.com>
2019-01-09 15:05:57 +0800

    Styled footer

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-  const page = viewUtils.load('index', 'main', { title: 'Index Page' });
+  const page = viewUtils.load('index', 'main', { title: 'Index Page', currentYear: (new Date()).getFullYear() });
   res.send(page);
 });
 
